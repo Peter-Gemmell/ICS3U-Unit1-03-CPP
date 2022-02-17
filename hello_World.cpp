e00ca40cd90ec91ecf8002d1220121dd73a3fb3a
Peter Gemmell <peter.gemmell@mths.ca>
2022-02-17 21:14:45 +0000

    February

---
+++ hello_World.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Peter Gemmell All rights reserved
+//
+// Created by Peter Gemmell
+// Created on February 2022
+// This is a "Hello world" program with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
